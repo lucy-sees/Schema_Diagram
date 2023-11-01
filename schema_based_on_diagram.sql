165e29a10aa10a2ea30a3fb636e4d92a89536892
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-11-01 16:18:24 +0100

    create medical_histories_treatment

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -42,10 +42,19 @@ CREATE TABLE invoice_items(
     FOREIGN KEY (treatment_id) REFERENCES treatments(id)
 );
 
+CREATE TABLE medical_histories_treatments(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    medical_histories_id INT,
+    treatments_id INT,
+    FOREIGN KEY (medical_histories_id) REFERENCES medical_histories(id),
+    FOREIGN KEY (treatments_id) REFERENCES treatments(id)
+);
+
 CREATE INDEX invoice_id_index ON invoice_items(invoice_id);
 CREATE INDEX treatment_id_index ON invoice_items(treatment_id);
 CREATE INDEX medical_history_id_index ON invoices (medical_history_id);
 CREATE INDEX patient_id_index ON medical_histories (patient_id);
+CREATE INDEX medical_histories_id_index ON medical_histories_treatments (medical_histories_id);
 CREATE INDEX treatments_id_index ON medical_histories_treatments (treatments_id);
 
 
